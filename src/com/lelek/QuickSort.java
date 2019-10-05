80ce41ae9d849c9234f3f4397bb2f4da74b2dd6e
Vasyl Lelek <itevas@outlook.com>
2019-10-05 11:09:58 +0300

    - static variables

--- src/com/lelek/QuickSort.java
+++ src/com/lelek/QuickSort.java
@@ -2,56 +2,54 @@ package com.lelek;
 
 public class QuickSort {
 
-    private static int[] array;
-    private static int arrayLength = 10;
-    private static int wall;
+    private static final int ARRAY_LENGTH = 10;
 
     public static void main(String[] args) {
-        array = ArrayGenerator.generate(arrayLength);
-        print();
-        System.out.println("Sorted " + check());
-        sort(0, arrayLength - 1);
-        print();
-        System.out.println("Sorted " + check());
+        int [] array = ArrayGenerator.generate(ARRAY_LENGTH);
+        print(array);
+        System.out.println("Sorted " + checkIfSorted(array));
+        sort(array, 0, array.length - 1);
+        print(array);
+        System.out.println("Sorted " + checkIfSorted(array));
     }
 
-    private static void sort(int startIndex, int endIndex) {
-        wall = preSortAndGetWall(startIndex, endIndex);
+    private static void sort(int[] array, int startIndex, int endIndex) {
+        int divisionIndex = getDivisionIndex(array, startIndex, endIndex);
         if (startIndex < endIndex) {
-            if (wall == startIndex) {
-                sort(++startIndex, endIndex);
-            } else if (wall == endIndex) {
-                sort(startIndex, --endIndex);
+            if (divisionIndex == startIndex) {
+                sort(array, ++startIndex, endIndex);
+            } else if (divisionIndex == endIndex) {
+                sort(array, startIndex, --endIndex);
             } else {
-                sort(startIndex, wall);
-                sort(wall + 1, endIndex);
+                sort(array, startIndex, divisionIndex);
+                sort(array, divisionIndex + 1, endIndex);
             }
         }
     }
 
-    private static int preSortAndGetWall(int start, int end) {
+    private static int getDivisionIndex(int[] array, int start, int end) {
         int pivot = array[end];
-        int wall = start - 1;
+        int divisionIndex = start - 1;
         for (int i = start; i <= end; i++) {
             if (array[i] <= pivot) {
-                wall++;
+                divisionIndex++;
                 int temp = array[i];
-                array[i] = array[wall];
-                array[wall] = temp;
+                array[i] = array[divisionIndex];
+                array[divisionIndex] = temp;
             }
         }
-        return wall;
+        return divisionIndex;
     }
 
-    private static void print() {
+    private static void print(int[] array) {
         for (int i = 0; i < array.length; i++) {
             System.out.print(array[i] + " ");
         }
         System.out.println();
     }
 
-    private static boolean check() {
-        for (int i = 0; i < arrayLength - 1; i++) {
+    private static boolean checkIfSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
             if (array[i] > array[i + 1]) {
                 return false;
             }
